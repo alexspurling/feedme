d129499222bdb3514874b723cec669d85a1079a0
Alex Spurling <alexspurling@gmail.com>
2010-04-21 22:04:42 +0100

    Restructuring project

---
+++ main.py
@@ -0,0 +1,15 @@
+from google.appengine.ext import webapp
+from google.appengine.ext.webapp.util import run_wsgi_app
+
+from feedme.views import *
+
+application = webapp.WSGIApplication(
+                                     [('/', MainPage),
+                                      ('/addfeed', AddFeed)],
+                                     debug=True)
+
+def main():
+    run_wsgi_app(application)
+
+if __name__ == "__main__":
+    main()
